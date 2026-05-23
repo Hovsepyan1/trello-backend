7bc42d039b63cc9d281c21dfefb4a9db7a36c11c
Sasha Hovsepyan <sashahovsepyan446@gmail.com>
2026-05-23 22:18:39 +0400

    Refactor: main

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-from app.routers import users
+from app.routers import users, boards, sections
 
 app = FastAPI()
 
 app.include_router(users.router, prefix="/users", tags=["users"])
+app.include_router(boards.router, prefix="/boards", tags=["boards"])
+app.include_router(sections.router, prefix="/sections", tags=["sections"])
+
 
 @app.get("/")
 async def get_home():
